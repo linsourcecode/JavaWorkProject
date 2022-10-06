4f4710be13b94fc696e24f69eafde63d5ec18be3
WIN-RT09V4GCT0I\Administrator <1679569188@qq.com>
2022-10-06 22:10:57 +0800

    ThreadLocal模块复习

---
+++ ThreadLocalDemo/src/main/java/com/example/Demo1.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Demo1 {
+    public static void main(String[] args) {
+
+    }
+}
+class t1{
+    ThreadLocal<String> t1=new ThreadLocal<String>();
+    public void setValue(){
+
+    }
+}
